ed714903a78fa90a93b47dc4b83171558df5788a
DufanD <quraishdufan@rocketmail.com>
2019-08-19 13:58:59 +0700

    initial commit

---
+++ src/main/java/com/finalproject/automated/refactoring/tool/detection/service/Detection.java
@@ -0,0 +1,20 @@
+package com.finalproject.automated.refactoring.tool.detection.service;
+
+import com.finalproject.automated.refactoring.tool.model.MethodModel;
+import lombok.NonNull;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author Dufan Quraish
+ * @version 1.0.0
+ * @since 19 August 2019
+ */
+
+public interface Detection {
+
+    Map<String, List<MethodModel>> detect(@NonNull String path);
+
+    Map<String, Map<String, List<MethodModel>>> detect(@NonNull List<String> paths);
+}
